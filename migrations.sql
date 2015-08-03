13a74daaf8fb6e4197cfb6823ea6e15fa7194977
James Haff <jehaff@James-Haffs-MacBook-Pro.local>
2015-08-03 16:19:24 -0500

    login

--- migrations.sql
+++ migrations.sql
@@ -1,13 +1,12 @@
-CREATE DATABASE bedroom 
+CREATE DATABASE bedroom
 
-\c bedroom 
+\c bedroom
 
 CREATE TABLE users (id SERIAL PRIMARY KEY, user_name VARCHAR(255), user_password VARCHAR(255), user_email VARCHAR(255), password_hash VARCHAR(255), password_salt VARCHAR(255) );
 CREATE TABLE pictures (id SERIAL PRIMARY KEY, picture_link TEXT);
 CREATE TABLE playlists (id SERIAL PRIMARY KEY, playlist_link TEXT);
-CREATE TABLE noises (id SERIAL PRIMARY KEY, noise_link TEXT); 
-CREATE TABLE environments (id SERIAL PRIMARY KEY,
-	FOREIGN KEY ( env_creator_name ) REFERENCES users (user_name),  
-	FOREIGN KEY ( env_picture_id ) REFERENCES pictures (id),
-	FOREIGN KEY ( env_playlist_id ) REFERENCES playlists (id),
-	FOREIGN KEY ( env_noise_id ) REFERENCES noises (id));
+CREATE TABLE noises (id SERIAL PRIMARY KEY, noise_link TEXT);
+CREATE TABLE bedrooms (id SERIAL PRIMARY KEY, env_creator_id INTEGER REFERENCES users (id),
+	env_picture_id INTEGER REFERENCES pictures (id),
+	env_playlist_id INTEGER REFERENCES playlists (id),
+	env_noise_id INTEGER REFERENCES noises (id));
