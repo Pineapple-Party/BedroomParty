87892280e7935d516cdcb594b1cf42bd6fd094ad
Dylan <invision33@gmail.com>
2015-08-05 16:57:54 -0500

    work om imgur js

--- public/scripts/imgur.js
+++ public/scripts/imgur.js
@@ -1,22 +1,25 @@
 $(document).ready(function() {
-  (function($){
+  
     var imageID = 'eYAJ7Ya';
     var galleryAPI = "https://api.imgur.com/3/gallery/image/" + imageID;
+    
+    function imgurCall() {
+      $.ajax({
+          url: galleryAPI,
+          headers:{
+              Authorization :'Client-ID 0fadbcbfac86bb4'
+          },
+          type: 'GET',
+          dataType: 'json',
+          success: function(data) { 
 
-    $.ajax({
-        url: galleryAPI,
-        headers:{
-            Authorization :'Client-ID 0fadbcbfac86bb4'
-        },
-        type: 'GET',
-        dataType: 'json',
-        success: function(data) { 
+              console.log(data.data.link);
 
-            console.log(data.data.link);
+          },
+          error: function() { console.log("ERRORZ"); }
+      });
+  }
+  var src = imgurCall() + ""; 
+  <img src=src>  
 
-        },
-        error: function() { console.log("ERRORZ"); }
-    });
-
-  })(jQuery);
 });
