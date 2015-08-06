a84100aaa3dc9529c91690124ffac0de10cd0c27
Dylan <invision33@gmail.com>
2015-08-06 15:03:26 -0500

    work on sharejs with building image gallery

--- public/scripts/share.js
+++ public/scripts/share.js
@@ -3,23 +3,24 @@ $(document).ready(function() {
  var share = share || {}; 
 
  function build_gallery() {
-    $('.picture-link').each(function(picture) {
-        pictures = []; 
-        pictures.push(picture); 
-    }); 
-    return pictures; 
- } 
+    var pic = $('.pictures-link'); 
+    var pictures = ($('.pictures-link').length - 1);
+    for (var i=0; i < pictures; i++) {
 
- 
- // share.noise = $('.noise-link').prop('value');
- share.noise = 'https://soundcloud.com/hhazydreamss/sets/telemarketing';
- console.log(share);  
+        console.log($(pic[i]).prop('value')); 
+    }
 
+    // $.each(pictures.prop('value'), function(picture) {
+    //     pictures.push($(picture).prop('value')); 
+    // }); 
+    // return pictures; 
+    }; 
 
  $('.share-btn').click(function() {
  	share.playlist = $('.playlist-link').prop('value');
     //share.noise = $('.noise-link').prop('value'); 
-    share.pictures = build_gallery(); 
+    share.pictures = build_gallery();
+    share.noise = $('#noiseSlider').prop('value'); 
  	$.ajax({
         type: "post",
         url: '/bedroom/share',
